87887d7c9a0d689fca7521a50ed9f0404c56f961
BlackOut 921 <conorhowe921@gmail.com>
2024-10-11 11:57:07 +0100

    Design WIP

--- wwwroot/Scripts/JavaScript.js
+++ wwwroot/Scripts/JavaScript.js
@@ -1,1 +1,8 @@
 ﻿"use strict"
+
+window.onload = () => {
+    const observer = new IntersectionObserver(entries =>
+        entries.forEach(i => i.isIntersecting && i.target.classList.add("show")));
+    const observerTargets = document.querySelectorAll(".observer-target");
+    observerTargets.length > 0 && observerTargets.forEach(i => observer.observe(i));
+};
